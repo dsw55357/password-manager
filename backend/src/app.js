937affc3721a279ec9ce4b36799635ca1374088f
Twoje Imię Nazwisko <twoj@email.com>
2026-03-29 00:08:15 +0100

    app.js updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use("/api/security", securityRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend działa");
@@ -14,3 +15,8 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
 });
+
+
+// curl -X POST http://localhost:3000/api/security/check-password-breach29? -H "Content-Type: application/json" -d '{"password":"password"}'
+// {"success":true,"pwned":true,"count":52256179,"message":"Hasło wystąpiło w publicznych bazach wycieków."}
+// Dla bardzo znanych słabych haseł wynik powinien zwrócić pwned: true i wysoki count, bo Pwned Passwords przechowuje liczbę wystąpień dla każdego hasha z korpusu wycieków.
